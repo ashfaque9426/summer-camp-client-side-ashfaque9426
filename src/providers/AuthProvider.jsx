c54ea19e53b3470ff8b96662e1b29da0be267136
ashfaque9426 <ashfaqsylvi9426@gmail.com>
2023-06-08 01:00:27 +0600

    AuthProvider created and useAuth custom hook added for easy access to auth context

---
+++ src/providers/AuthProvider.jsx
@@ -0,0 +1,75 @@
+// eslint-disable-next-line no-unused-vars
+import React, { createContext, useEffect, useState } from 'react';
+import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth"
+import app from '../firebase/firebase.config';
+import axios from 'axios';
+
+export const AuthContext = createContext(null);
+const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
+
+const AuthProvider = ({children}) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    const createUser = (email, password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+
+    const signInWithEP = (email, password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+
+    const updateUserProfile = (name, photo) => {
+        setLoading(true);
+        return updateProfile(auth.currentUser, {
+            displayName: name, photoURL: photo
+        });
+    }
+
+    const googleSignIn = () => {
+        setLoading(true);
+        return signInWithPopup(auth, googleProvider);
+    }
+
+    const logOut = () => {
+        setLoading(true);
+        return signOut(auth);
+    }
+
+    const authInfo = {
+        user,
+        loading,
+        createUser,
+        signInWithEP,
+        updateUserProfile,
+        googleSignIn,
+        logOut
+    }
+
+    useEffect(()=> {
+        const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            if(currentUser) {
+                setUser(currentUser);
+                axios.post('http://localhost:5000/jwt', { email: currentUser?.email })
+                .then(data => {
+                    localStorage.setItem('summer-token', data.data);
+                    setLoading(false);
+                })
+            }
+            else {
+                localStorage.removeItem('summer-token');
+            }
+
+            return () => unsubscribe();
+        });
+    })
+
+    return (
+        <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
