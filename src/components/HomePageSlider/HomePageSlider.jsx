99267c597f5bc1f60aae4f8940ff40ac0597d993
ashfaque9426 <ashfaqsylvi9426@gmail.com>
2023-07-09 01:24:46 +0600

    Some optimizations have made on homepage carousel height

--- src/components/HomePageSlider/HomePageSlider.jsx
+++ src/components/HomePageSlider/HomePageSlider.jsx
@@ -9,15 +9,15 @@ import camp3 from '../../assets/bannerImages/camp3.jpg';
 const HomePageSlider = () => {
     return (
         <Carousel className='text-center'>
-            <div>
+            <div className='lg:h-[calc(100vh-15vh)]'>
                 <img src={camp1} />
                 <p className="legend">Welcome to Shutter Safari</p>
             </div>
-            <div>
+            <div className='lg:h-[calc(100vh-15vh)]'>
                 <img src={camp2} />
                 <p className="legend">Achive your goal of being a photographer during your summer. Make the summer fun and enjoy learning photography according to your taste</p>
             </div>
-            <div>
+            <div className='lg:h-[calc(100vh-15vh)]'>
                 <img src={camp3} />
                 <p className="legend">You can learn what you prefer. So, what do you want to be? A wild life photographer or you just like Potraits or landscapes? Excited?</p>
             </div>
